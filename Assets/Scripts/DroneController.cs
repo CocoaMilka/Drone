6981edb9f14a567fe549f3d4d695983102d47b09
Cocoa Milka <59587889+CocoaMilka@users.noreply.github.com>
2023-09-21 13:17:14 -0500

    refactored code, physics still a bit wonky

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -1,17 +1,15 @@
 using System.Collections.Generic;
-using System.Data;
 using UnityEngine;
 
 public class DroneController : MonoBehaviour
 {
     bool isOn = false;
-
-    // Force will be applied from propellers
     public List<GameObject> props;
 
-    public float distance = 5f;
-    public float force = 0.1f;
-    public float turnSpeed = 10f;
+    // Speed of gravity is set to 10, this is to allow hover
+    public float baseForce = 10f;
+    public float forwardForceMultiplier = 5f;
+    public float turnTorqueMultiplier = 5f;
 
     // Controller Input
     public float horizontalSensitivity = 0.1f;
@@ -44,58 +42,46 @@ public class DroneController : MonoBehaviour
 
     private void FixedUpdate()
     {
-
         if (isOn)
         {
-            force = 10;
-            float forceHorizontal = 0;
-
-            // Read joystick input
-            float horizontal = Input.GetAxis("Horizontal_R") * horizontalSensitivity;
-            float vertical = Input.GetAxis("Vertical_L") * verticalSensitivity;
-            float forward = Input.GetAxis("Forward_R") * verticalSensitivity;
-
-            force += vertical;
-            Debug.Log(force);
-
-            forceHorizontal += forward;
-            body.AddForce(transform.TransformDirection(Vector3.forward) * forceHorizontal);
-
-            // For turning
-            body.AddTorque(transform.TransformDirection(Vector3.up) * horizontal * turnSpeed);
-
-            // Apply upwards force from each propeller
-            foreach (GameObject prop in props)
-            {
-                body.AddForceAtPosition(transform.TransformDirection(Vector3.up) * force / 4, prop.transform.position);
-
-                /*
-                RaycastHit hit;
-                if (Physics.Raycast(prop.transform.position, transform.TransformDirection(Vector3.down), out hit, distance))
-                {
-                    //body.AddForceAtPosition(transform.TransformDirection(Vector3.up) * Mathf.Pow(distance - hit.distance, 2) / distance * force, prop.transform.position);
-                    body.AddForceAtPosition(transform.TransformDirection(Vector3.up) * force / 4, prop.transform.position);
-
-                    // Debug raycast
-                    Debug.DrawRay(prop.transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.green);
-                }
-                */
-            }
+            HandleInput();
+            ApplyPhysics();
         }
     }
 
-    // Turn on drone rotors and activates physics
+    private void HandleInput()
+    {
+        // Read joystick input
+        float horizontalInput = Input.GetAxis("Horizontal_R") * horizontalSensitivity;
+        float verticalInput = Input.GetAxis("Vertical_L") * verticalSensitivity;
+        float forwardInput = Input.GetAxis("Forward_R") * verticalSensitivity;
+
+        // Update force and torque based on input
+        baseForce = 10 + verticalInput;
+        Debug.Log(baseForce);
+
+        body.AddForce(transform.forward * forwardInput * forwardForceMultiplier);
+        body.AddTorque(transform.up * horizontalInput * turnTorqueMultiplier);
+    }
+
+    private void ApplyPhysics()
+    {
+        // Apply upwards force from each propeller
+        foreach (GameObject prop in props)
+        {
+            body.AddForceAtPosition(transform.TransformDirection(Vector3.up) * baseForce / 4, prop.transform.position);
+        }
+    }
+
     void startDrone()
     {
         isOn = true;
-
-        // Spin rotors
+        // Add logic to spin rotors
     }
 
     void stopDrone()
     {
         isOn = false;
-
-        // Turn off rotors
+        // Add logic to turn off rotors
     }
 }
