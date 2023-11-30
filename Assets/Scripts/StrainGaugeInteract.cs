9f4a70ab61ffca07841983fcba16d840a44e877a
Cocoa Milka <59587889+CocoaMilka@users.noreply.github.com>
2023-11-30 13:32:43 -0600

    add interaction interface ; make render on top ; bug when multiple robots in collider, perhaps check for isSelected?

--- Assets/Scripts/StrainGaugeInteract.cs
+++ Assets/Scripts/StrainGaugeInteract.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class StrainGaugeInteract : MonoBehaviour
 {
     public GameObject strainDisplay;
+    public bool isInteractable;
 
+    private Transform collidedObjectTransform;
     private void Start()
     {
         strainDisplay.SetActive(false);
@@ -14,15 +16,27 @@ public class StrainGaugeInteract : MonoBehaviour
     private void Update()
     {
         strainDisplay.transform.rotation = Camera.main.transform.rotation;
+
+        // Display slightly below robot
+        strainDisplay.transform.position = collidedObjectTransform.position - new Vector3(0, 2, 0);
+
+
+        if (isInteractable && Input.GetButtonDown("Interact"))
+        {
+            Debug.Log("interacting...");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        isInteractable = true;
+        collidedObjectTransform = other.transform; // Store the transform of the collided object
         strainDisplay.SetActive(true);
     }
 
     private void OnTriggerExit(Collider other)
     {
+        isInteractable = false;
         strainDisplay.SetActive(false);
     }
 }
